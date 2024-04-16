ddef4c71562b1479dc1ff1b4ea8b2ef2703be915
Jordan Tran <jordan.tran@student.manchester.ac.uk>
2024-04-16 13:45:05 +0100

    update engine options

--- src/components/EngineSelector.tsx
+++ src/components/EngineSelector.tsx
@@ -16,8 +16,11 @@ const EngineSelector: React.FC<EngineSelectorProps> = ({ selectedEngine, onEngin
       <label htmlFor="engine-select">Select Engine:</label>
       <select id="engine-select" value={selectedEngine} onChange={handleEngineChange}>
         <option value="stockfish">Stockfish</option>
-        <option value="mistral-7b">Mistral-7B</option>
-        <option value="mixtral-8x7b">Mixtral-8x7B</option>
+        <option value="open-mistral-7b">Mistral-7B</option>
+        <option value="open-mixtral-8x7b">Mixtral-8x7B</option>
+        <option value="mistral-small-latest">Mistral-Small-Latest</option>
+        <option value="mistral-medium-latest">Mistral-Medium-Latest</option>
+        <option value="mistral-large-latest">Mistral-Large-Latest</option>
       </select>
     </div>
   );
